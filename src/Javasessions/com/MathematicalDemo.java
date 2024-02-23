8134e886cae2f3c8e1fa739d72deece1494e28c9
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2024-02-23 08:46:45 +0530

    Mathematical demo

---
+++ src/Javasessions/com/MathematicalDemo.java
@@ -0,0 +1,70 @@
+package Javasessions.com;
+
+public class MathematicalDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("*******************************");	
+		int a=10;
+		int b=30;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		//System.out.println(a=b);
+		
+		System.out.println(a%b);
+		
+		System.out.println(a%b);
+		
+	//**************************************************	
+		
+		//String--Any thing which is enclosed with in double quotes
+		
+		System.out.println("*******************************");	
+		String s="Welcome nucot";
+		System.out.println(s);
+		
+		String ss="Bangalore";
+		System.out.println(ss);
+		
+		System.out.println(s+ss);
+		
+		System.out.println(a+s);
+		System.out.println(a+b+s);
+		System.out.println(a+b+s+ss);
+		System.out.println(s+ss+a+b);
+		System.out.println(s+ss+(a+b));
+		
+		System.out.println("*******************************");	
+		
+		char c='a';
+		char cc='b';
+		
+		
+		
+		System.out.println(s+ss);
+		System.out.println(c+s);
+		System.out.println(c+cc+s);
+		System.out.println(c+cc+s+ss);
+		System.out.println(s+ss+c+cc);
+		System.out.println(s+ss+(c+cc));
+		
+	System.out.println("*******************************");	
+		short sh=14;
+		short shh=10;
+		System.out.println(s+ss);
+		System.out.println(sh+s);
+		System.out.println(sh+shh+s);
+		System.out.println(sh+shh+s+ss);
+		System.out.println(s+ss+sh+shh);
+		System.out.println(s+ss+(sh+shh));
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
