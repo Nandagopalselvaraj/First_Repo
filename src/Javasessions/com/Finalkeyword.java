defa6c12809ca315ee8dda80ac82c15e8090ce01
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2024-02-26 08:51:30 +0530

    Conditional statements

---
+++ src/Javasessions/com/Finalkeyword.java
@@ -0,0 +1,32 @@
+package Javasessions.com;
+
+public class Finalkeyword {
+
+	public static void main(String[] args) {
+		
+		int a=5;
+			a=6;
+			a=10;
+		System.out.println(a);
+		
+		
+		final int days=7;
+					
+		System.out.println(days);
+		
+		final float pi=3.14f;
+		System.out.println(pi);
+		
+	final	String companyName="Nucot";
+	System.out.println(companyName);
+		
+	
+  final	char genderMale='m';
+  System.out.println(genderMale);
+  final	char genderFemale='f';	
+		
+		
+
+	}
+
+}
