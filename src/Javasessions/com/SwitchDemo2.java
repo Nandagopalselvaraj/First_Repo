ee8d07a558f4d8034db52af27a1847ca0dffd7a8
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2024-02-27 08:40:46 +0530

    Switch Demo

---
+++ src/Javasessions/com/SwitchDemo2.java
@@ -0,0 +1,47 @@
+package Javasessions.com;
+
+public class SwitchDemo2 {
+
+	public static void main(String[] args) {
+
+		char c='u';
+		switch(c)
+		{
+		
+				case 'a': 
+		
+					System.out.println("a is vowel");
+					break;
+				case 'e': 
+					System.out.println("e is vowel");
+					break;
+				case 'i': 
+					
+					System.out.println("i is vowel");
+					break;
+				case 'o': 
+					
+					System.out.println("o is vowel");
+					break;
+		
+				case 'u': 
+					
+					System.out.println("u is vowel");
+					break;
+		
+				default:
+					System.out.println("This consonant");
+					break;
+		
+		
+		
+		
+		}
+		
+			System.out.println("Thanks");
+			System.out.println("Thanks");
+			System.out.println("Thanks");
+
+	}
+
+}
---
+++ src/Javasessions/com/SwitchDemo3.java
@@ -0,0 +1,47 @@
+package Javasessions.com;
+
+public class SwitchDemo3 {
+	
+	public static void main(String[] args) {
+
+		int num=4;
+		switch(num)
+		{
+		
+				case 1: 
+		
+					System.out.println("result of 1 is 100");
+					break;
+				case 2: 
+					System.out.println("result of 2 is 80");
+					break;
+				case 3: 
+					
+					System.out.println("result of 3 is 65");
+					break;
+				case 4: 
+					
+					System.out.println("result of 4 is 35");
+					break;
+		
+				case 5: 
+					
+					System.out.println("result of 5 is 95");
+					break;
+		
+				default:
+					System.out.println("Plz pass correct usn number");
+					break;
+		
+		
+		
+		
+		}
+		
+			System.out.println("Thanks");
+			System.out.println("Thanks");
+			System.out.println("Thanks");
+
+	}
+
+}
