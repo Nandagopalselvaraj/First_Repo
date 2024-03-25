09d54cdf19e27a769cdb4fdb5a61ea387606b53c
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2024-03-25 09:25:22 +0530

    Hashmap

---
+++ src/CollectionsDemo/com/MapConcept.java
@@ -0,0 +1,44 @@
+package CollectionsDemo.com;
+
+import java.util.HashMap;
+
+public class MapConcept {
+
+	public static void main(String[] args) {
+		
+		HashMap<String,Integer> omap=new HashMap<String,Integer>();
+		
+		omap.put("Akhila", 20);
+		omap.put("Nanda", 22);
+		omap.put("Praveen", 25);
+		omap.put("Pritham", 23);
+		omap.put("sashikala", 12);
+		System.out.println(omap.size());
+		System.out.println("Printing all the values");
+		
+		for(String names:omap.keySet())
+		{
+			System.out.println(omap.get(names));
+			
+		}
+		System.out.println("Getting the values based the key");
+		
+		System.out.println(omap.get("Akhila"));
+		System.out.println(omap.get("Praveen"));
+		
+		
+		if(omap.containsValue(200))
+		{
+			System.out.println("This is the value of akhila");
+		}
+		
+		System.out.println("Thanks");
+		
+		omap.remove("Nanda");
+		System.out.println(omap.size());
+		
+		
+
+	}
+
+}
